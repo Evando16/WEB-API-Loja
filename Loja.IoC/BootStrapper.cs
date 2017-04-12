05e808589ef0da7ff91598b7c6d7c2713ce21c15
Evando16 <evando66.ea@gmail.com>
2017-04-12 10:41:06 -0300

    WEB API Loja.IoC (dependency injection)
    The SimpleInjector was used to DI
    
    https://www.nuget.org/packages/SimpleInjector/

---
+++ Loja.IoC/BootStrapper.cs
@@ -0,0 +1,36 @@
+﻿using Business;
+using Loja.Business.Interfaces;
+using Loja.Repository;
+using Loja.Repository.Interfaces;
+using SimpleInjector;
+
+namespace Loja.IoC
+{
+    public class BootStrapper
+    {
+        public static void Resolve(Container container)
+        {
+            // For instance:
+            //Pessoa
+            container.Register<IPessoaBO, PessoaBO>(Lifestyle.Scoped);
+            container.Register<IPessoaRepository, PessoaRepository>(Lifestyle.Scoped);
+
+            //Fornecedor
+            container.Register<IFornecedorBO, FornecedorBO>(Lifestyle.Scoped);
+            container.Register<IFornecedorRepository, FornecedorRepository>(Lifestyle.Scoped);
+
+            //Produto
+            container.Register<IProdutoBO, ProdutoBO>(Lifestyle.Scoped);
+            container.Register<IProdutosRepository, ProdutoRepository>(Lifestyle.Scoped);
+
+            //Estoque
+            container.Register<IEstoqueBO, EstoqueBO>(Lifestyle.Scoped);
+            container.Register<IEstoqueRepository, EstoqueRepository>(Lifestyle.Scoped);
+
+            //Venda
+            container.Register<IVendaBO, VendaBO>(Lifestyle.Scoped);
+            container.Register<IVendaRepository, VendaRepository>(Lifestyle.Scoped);
+
+        }
+    }
+}
