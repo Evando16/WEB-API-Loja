c47ccc64e8efc39d8b0c897c8e8e9f8477d7ec3b
Evando16 <evando66.ea@gmail.com>
2017-04-12 10:43:44 -0300

    WEB API Loja.MyMapper (AutoMapper)

---
+++ Loja.MyMappper/AutoMapperConfig.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Loja.ClasseLibrary;
+using Loja.DTO;
+
+namespace Loja.MyMappper
+{
+    public static class AutoMapperConfig
+    {
+        public static T MapTo<T>(this object from)
+        {
+            if (from == null)
+                return default(T);
+
+            return Mapper.Map<T>(from);
+        }
+
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMissingTypeMaps = true;
+                // other configurations that not use the same properties
+
+                cfg.CreateMap<Cliente, ClienteDTO>().ReverseMap();
+                cfg.CreateMap<Vendedor, VendedorDTO>().ReverseMap();
+
+                cfg.CreateMap<PessoaDTO, ClienteDTO>().ReverseMap();
+                cfg.CreateMap<PessoaDTO, VendedorDTO>().ReverseMap();
+                cfg.CreateMap<Pessoa, PessoaDTO>().ReverseMap();
+
+                cfg.CreateMap<Fornecedor, FornecedorDTO>().ReverseMap();
+                cfg.CreateMap<Produto, ProdutoDTO>().ReverseMap();
+                cfg.CreateMap<Estoque, EstoqueDTO>().ReverseMap();
+                cfg.CreateMap<Venda, VendaDTO>().ReverseMap();
+                cfg.CreateMap<ItemVenda, ItemVendaDTO>().ReverseMap();
+
+
+            });
+        }
+    }
+}
